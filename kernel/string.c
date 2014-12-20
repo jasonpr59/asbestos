5b7c09b39f8c02a44da06f3f3ea030948ac20f64
Jason P-R <jasonpr59@gmail.com>
2014-12-20 00:10:53 -0500

    Add string.c and string.h.
    They only contain memmove, for now!
    
    Memmove will be required for terminal scrolling in an upcoming commit.

---
+++ kernel/string.c
@@ -0,0 +1,25 @@
+#include "string.h"
+
+void* memmove(void *dest, const void *src, size_t count) {
+  char *src_char = (char *) src;
+  char *dest_char = (char *) dest;
+
+  char *from;
+  char *to;
+  if (src >= dest) {
+    // Copy front-to-back.
+    from = src_char;
+    to = dest_char;
+    while (from < src_char + count) {
+      *to++ = *from++;
+    }
+  } else {
+    // Copy back-to-front.
+    from = src_char + count - 1;
+    to = dest_char + count - 1;
+    while (from >= src_char) {
+      *to-- = *from--;
+    }
+  }
+  return dest;
+}
---
+++ kernel/string.h
@@ -0,0 +1,8 @@
+#ifndef ASBESTOS_STRING_H_
+#define ASBESTOS_STRING_H_
+
+#include <stddef.h>
+
+void* memmove(void *dest, const void *src, size_t count);
+
+#endif  // ASBESTOS_STRING_H_
