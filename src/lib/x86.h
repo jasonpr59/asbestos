41d8c414ad442787d48b916f03d8c4b95fbf3cd9
Jason P-R <jasonpr59@gmail.com>
2014-12-21 16:56:42 -0500

    Use uint16_t for outb port spec.

--- src/lib/x86.h
+++ src/lib/x86.h
@@ -9,12 +9,12 @@
 // want our instruction wrapper functions to be inlined.  So, we mark
 // all of them with always_inline.
 
-static __inline void outb(int io_port, uint8_t data) __attribute__((always_inline));
+static __inline void outb(uint16_t io_port, uint8_t data) __attribute__((always_inline));
 
 // The OUTB instruction constrains which registers we use.
 // See http://x86.renejeschke.de/html/file_module_x86_id_222.html.
-static void outb(int io_port, uint8_t data) {
-  __asm __volatile("outb %0, %w1" :
+static void outb(uint16_t io_port, uint8_t data) {
+  __asm __volatile("outb %0, %1" :
 		   /* No output. */ : 
 		   "a" (data), "d" (io_port));
 }
