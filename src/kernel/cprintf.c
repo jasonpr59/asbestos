b626ea46493985e11e2f39560dd9b741b0ebfd4d
Jason P-R <jasonpr59@gmail.com>
2014-12-21 18:39:03 -0500

    Implement cprintf!
    And, replace vga_write_string() calls with cprintf() calls.

---
+++ src/kernel/cprintf.c
@@ -0,0 +1,14 @@
+#include <stdarg.h>
+#include <print.h>
+#include "cprintf.h"
+#include "vga.h"
+
+void print_char(char character) {
+  vga_write(character);
+}
+
+void cprintf(char *format, ...) {
+  va_list data;
+  va_start(data, format);
+  emit_formatted_var(print_char, format, data);
+}
--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,38 +1,39 @@
+#include "cprintf.h"
 #include "vga.h"
 
 void vga_demo_scroll() {
   // TODO(jasonpr): Replace with a loop once cprintf is implemented!
-  vga_write_string("Demo scroll, line 0.\n");
-  vga_write_string("Demo scroll, line 1.\n");
-  vga_write_string("Demo scroll, line 2.\n");
-  vga_write_string("Demo scroll, line 3.\n");
-  vga_write_string("Demo scroll, line 4.\n");
-  vga_write_string("Demo scroll, line 5.\n");
-  vga_write_string("Demo scroll, line 6.\n");
-  vga_write_string("Demo scroll, line 7.\n");
-  vga_write_string("Demo scroll, line 8.\n");
-  vga_write_string("Demo scroll, line 9.\n");
-  vga_write_string("Demo scroll, line 10.\n");
-  vga_write_string("Demo scroll, line 11.\n");
-  vga_write_string("Demo scroll, line 12.\n");
-  vga_write_string("Demo scroll, line 13.\n");
-  vga_write_string("Demo scroll, line 14.\n");
-  vga_write_string("Demo scroll, line 15.\n");
-  vga_write_string("Demo scroll, line 16.\n");
-  vga_write_string("Demo scroll, line 17.\n");
-  vga_write_string("Demo scroll, line 18.\n");
-  vga_write_string("Demo scroll, line 19.\n");
-  vga_write_string("Demo scroll, line 20.\n");
-  vga_write_string("Demo scroll, line 21.\n");
-  vga_write_string("Demo scroll, line 22.\n");
-  vga_write_string("Demo scroll, line 23.\n");
-  vga_write_string("Demo scroll, line 24.\n");
-  vga_write_string("Demo scroll, line 25.\n");
-  vga_write_string("Demo scroll, line 26.\n");
-  vga_write_string("Demo scroll, line 27.\n");
-  vga_write_string("Demo scroll, line 28.\n");
-  vga_write_string("Demo scroll, line 29.\n");
-  vga_write_string("Demo scroll, line 30.\n");
+  cprintf("Demo scroll, line 0.\n");
+  cprintf("Demo scroll, line 1.\n");
+  cprintf("Demo scroll, line 2.\n");
+  cprintf("Demo scroll, line 3.\n");
+  cprintf("Demo scroll, line 4.\n");
+  cprintf("Demo scroll, line 5.\n");
+  cprintf("Demo scroll, line 6.\n");
+  cprintf("Demo scroll, line 7.\n");
+  cprintf("Demo scroll, line 8.\n");
+  cprintf("Demo scroll, line 9.\n");
+  cprintf("Demo scroll, line 10.\n");
+  cprintf("Demo scroll, line 11.\n");
+  cprintf("Demo scroll, line 12.\n");
+  cprintf("Demo scroll, line 13.\n");
+  cprintf("Demo scroll, line 14.\n");
+  cprintf("Demo scroll, line 15.\n");
+  cprintf("Demo scroll, line 16.\n");
+  cprintf("Demo scroll, line 17.\n");
+  cprintf("Demo scroll, line 18.\n");
+  cprintf("Demo scroll, line 19.\n");
+  cprintf("Demo scroll, line 20.\n");
+  cprintf("Demo scroll, line 21.\n");
+  cprintf("Demo scroll, line 22.\n");
+  cprintf("Demo scroll, line 23.\n");
+  cprintf("Demo scroll, line 24.\n");
+  cprintf("Demo scroll, line 25.\n");
+  cprintf("Demo scroll, line 26.\n");
+  cprintf("Demo scroll, line 27.\n");
+  cprintf("Demo scroll, line 28.\n");
+  cprintf("Demo scroll, line 29.\n");
+  cprintf("Demo scroll, line 30.\n");
 }
 
 void kernel_initialize() {
