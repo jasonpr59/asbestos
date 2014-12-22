596bcdbe0e9d3fc9e32816dbe0001f96a37c77f1
Jason P-R <jasonpr59@gmail.com>
2014-12-22 00:49:16 -0500

    Update kernel demo functinos to use cprintf for displaying numbers.

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -2,42 +2,13 @@
 #include "vga.h"
 
 void vga_demo_scroll() {
-  // TODO(jasonpr): Replace with a loop once cprintf is implemented!
-  cprintf("Demo scroll, line 0.\n");
-  cprintf("Demo scroll, line 1.\n");
-  cprintf("Demo scroll, line 2.\n");
-  cprintf("Demo scroll, line 3.\n");
-  cprintf("Demo scroll, line 4.\n");
-  cprintf("Demo scroll, line 5.\n");
-  cprintf("Demo scroll, line 6.\n");
-  cprintf("Demo scroll, line 7.\n");
-  cprintf("Demo scroll, line 8.\n");
-  cprintf("Demo scroll, line 9.\n");
-  cprintf("Demo scroll, line 10.\n");
-  cprintf("Demo scroll, line 11.\n");
-  cprintf("Demo scroll, line 12.\n");
-  cprintf("Demo scroll, line 13.\n");
-  cprintf("Demo scroll, line 14.\n");
-  cprintf("Demo scroll, line 15.\n");
-  cprintf("Demo scroll, line 16.\n");
-  cprintf("Demo scroll, line 17.\n");
-  cprintf("Demo scroll, line 18.\n");
-  cprintf("Demo scroll, line 19.\n");
-  cprintf("Demo scroll, line 20.\n");
-  cprintf("Demo scroll, line 21.\n");
-  cprintf("Demo scroll, line 22.\n");
-  cprintf("Demo scroll, line 23.\n");
-  cprintf("Demo scroll, line 24.\n");
-  cprintf("Demo scroll, line 25.\n");
-  cprintf("Demo scroll, line 26.\n");
-  cprintf("Demo scroll, line 27.\n");
-  cprintf("Demo scroll, line 28.\n");
-  cprintf("Demo scroll, line 29.\n");
-  cprintf("Demo scroll, line 30.\n");
+  for (int i = 0; i < 30; i++) {
+    cprintf("Demo scroll, line 0x%02x.\n", i);
+  }
 }
 
 void demo_cprintf() {
-  cprintf("The hex number %x is twenty.\n", 20);
+  cprintf("The hex number 0x%x is twenty.\n", 20);
   cprintf("The word '%5s' means '%15s' in Spanish.\n",
 	  "hola", "hello");
   cprintf("The word '%s' means '%s' in Spanish.\n",
