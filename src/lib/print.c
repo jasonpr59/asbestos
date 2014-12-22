cdb8d9c00a5a3452ea4ad8af6b651fc0088df2e9
Jason P-R <jasonpr59@gmail.com>
2014-12-21 19:01:55 -0500

    Write skeleton for handling args in emit_formatted_var.
    Only escaped percentages, e.g. "100%%", are actually implemented.

--- src/lib/print.c
+++ src/lib/print.c
@@ -7,10 +7,38 @@ void emit_formatted(void (*emit)(char), char *format, ...) {
   va_end(data);
 }
 
+// Emit the data for the current escape sequence, partly consuming the
+// format and data streams.
+void emit_datum(void (*emit)(char), char *format_stream,
+		va_list data_stream) {
+  char data_type = *format_stream++;
+  switch (data_type) {
+  case '%':
+    // It's just an escaped percent sign.
+    emit('%');
+    return;
+  case 'x':
+    // Emit as hex.
+    // TODO(jasonpr): Implement.
+    return;
+  case 's':
+    // Emit string.
+    // TODO(jasonpr): Implement.
+    return;
+  default:
+    // TODO(jasonpr): Throw an error.
+    return;
+  }
+}
+
 void emit_formatted_var(void (*emit)(char), char *format, va_list data) {
-  // TODO(jasonpr): Actually do some formatting!
-  while (*format) {
-    emit(*format);
-    format++;
+  char *next_char = format;
+  while (*next_char) {
+    if (*next_char == '%') {
+      next_char++;
+      emit_datum(emit, next_char, data);
+      continue;
+    }
+    emit(*next_char++);
   }
 }  
