e467025fa1f529a6bb1582db3d64058773317c9e
Jason P-R <jasonpr59@gmail.com>
2014-12-25 20:45:09 -0500

    Implement keyboard driver.

---
+++ src/kernel/keyboard.c
@@ -0,0 +1,158 @@
+// This file implements keyboard control.
+
+// Useful resources include:
+//  * zet.aluzina.org/images/d/d4/8042.pdf
+//  * stanislavs.org/helppc/8042.html
+//  * The IBM PC AT Technical Reference (1502494), available online at
+//    ia801607.us.archive.org/30/items/bitsavers_ibmpcat150ferenceMar84_26847525/1502494_PC_AT_Technical_Reference_Mar84.pdf
+
+#include <ringbuffer.h>
+#include <stdbool.h>
+#include <x86.h>
+#include "cprintf.h"
+#include "keyboard.h"
+
+
+#define KEYBOARD_DATA_IO_PORT 0x60
+#define KEYBOARD_STATUS_IO_PORT 0x64
+#define KEYBOARD_COMMAND_IO_PORT 0x64
+
+#define KEYBOARD_OUTPUT_BUFFER_FULL 0x1
+#define KEYBOARD_INPUT_BUFFER_FULL 0x2
+
+#define KEYBOARD_SELF_TEST_COMMAND 0xAA
+#define KEYBOARD_SELF_TEST_EXPECTED_RESULT 0x55
+
+#define KEYBOARD_INTERFACE_TEST_COMMAND 0xAB
+#define KEYBOARD_INTERFACE_TEST_EXPECTED_RESULT 0x00
+
+#define CHARACTER_RING_SIZE 100
+
+static const char kScancodeLeftShiftPressed = 0x2A;
+static const char kScancodeLeftShiftReleased = 0xAA;
+static const char kScancodeRightShiftPressed = 0x36;
+static const char kScancodeRightShiftReleased = 0xB6;
+
+
+static char key_to_ascii[256] =
+{
+  0x0, 0x0, '1', '2', '3', '4', '5', '6',
+  '7', '8', '9', '0', '-', '=', 0x0, 0x0,
+  'q', 'w', 'e', 'r', 't', 'y', 'u', 'i',
+  'o', 'p', '[', ']', '\n', 0x0, 'a', 's',
+  'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',
+  '\'', '`', 0x0, '\\', 'z', 'x', 'c', 'v',
+  'b', 'n', 'm', ',', '.', '/', 0x0, '*',
+  0x0, ' ', 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+  0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, '7',
+  '8', '9', '-', '4', '5', '6', '+', '1', 
+  '2', '3', '0', '.'
+};
+
+static char key_to_ascii_shift[256] =
+{
+  0x0, 0x0, '!', '@', '#', '$', '%', '^',
+  '&', '*', '(', ')', '_', '+', 0x0, 0x0,
+  'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I',
+  'O', 'P', '{', '}', '\n', 0x0, 'A', 'S',
+  'D', 'F', 'G', 'H', 'J', 'K', 'L', ':',
+  '"', '~', 0x0, '|', 'Z', 'X', 'C', 'V',
+  'B', 'N', 'M', '<', '>', '?', 0x0, '*',
+  0x0, ' ', 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+  0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, '7',
+  '8', '9', '-', '4', '5', '6', '+', '1', 
+  '2', '3', '0', '.'
+};
+
+// Keyboard state.
+static char character_ring_space[CHARACTER_RING_SIZE];
+static struct RingBuffer character_buffer = {
+  character_ring_space,
+  CHARACTER_RING_SIZE,
+  0,
+  0
+};
+static bool left_shift_pressed = false; 
+static bool right_shift_pressed = false; 
+
+void spin_until_input_buffer_empty() {
+  while (inb(KEYBOARD_STATUS_IO_PORT) & KEYBOARD_INPUT_BUFFER_FULL) {
+    // Do nothing.  Just spin.
+  }
+}
+
+void spin_until_output_buffer_full() {
+  while (!(inb(KEYBOARD_STATUS_IO_PORT) & KEYBOARD_OUTPUT_BUFFER_FULL)) {
+    // Do nothing.  Just spin.
+  }
+}
+
+void keyboard_write_command(char command) {
+  spin_until_input_buffer_empty();
+  outb(KEYBOARD_COMMAND_IO_PORT, command);
+}
+
+char keyboard_read_data() {
+  spin_until_output_buffer_full();
+  return inb(KEYBOARD_DATA_IO_PORT);
+}
+
+void keyboard_self_test() {
+  keyboard_write_command(KEYBOARD_SELF_TEST_COMMAND);
+  char self_test_result = keyboard_read_data();
+  if (self_test_result != KEYBOARD_SELF_TEST_EXPECTED_RESULT) {
+    // TODO(jasonpr): Panic.
+    cprintf("[KEYBOARD] Error: Failed self test.\n");
+  }
+}
+
+void keyboard_interface_test() {
+  keyboard_write_command(KEYBOARD_INTERFACE_TEST_COMMAND);
+  char interface_test_result = keyboard_read_data();
+  if (interface_test_result != KEYBOARD_INTERFACE_TEST_EXPECTED_RESULT) {
+    // TODO(jasonpr): Panic.
+    cprintf("[KEYBOARD] Error: Failed interface test.\n");
+  }
+}
+
+void keyboard_initialize() {
+  keyboard_self_test();
+  keyboard_interface_test();
+}
+
+// Return zero if the keypress was successfully consumed, -1 otherwise.
+// A failure will occur if the kernel's key buffer is full.
+int consume_keypress() {
+  if (ring_buffer_full(&character_buffer)) {
+    // Fail immediately, even though it's possible that the key will
+    // not require adding to the buffer.
+    return -1;
+  }
+  char scancode = keyboard_read_data();
+  if (scancode == kScancodeLeftShiftPressed) {
+    left_shift_pressed = true;
+  } else if (scancode == kScancodeLeftShiftReleased) {
+    left_shift_pressed = false;
+  } else if (scancode == kScancodeRightShiftPressed) {
+    right_shift_pressed = true;
+  } else if (scancode == kScancodeRightShiftReleased) {
+    right_shift_pressed = false;
+  }
+  size_t scancode_index = (size_t) scancode;
+  char ascii_character = (left_shift_pressed || right_shift_pressed)
+      ? key_to_ascii_shift[scancode_index]
+      : key_to_ascii[scancode_index];
+  if (ascii_character) {
+    ring_buffer_append(&character_buffer, ascii_character);
+  }
+  // Otherwise, there was no ASCII value for this key.  It must have
+  // been special.  For now, ignore it.
+  return 0;
+}
+
+char keyboard_read() {
+  while (ring_buffer_empty(&character_buffer)) {
+    consume_keypress();
+  }
+  return ring_buffer_pop(&character_buffer);
+}
---
+++ src/kernel/keyboard.h
@@ -0,0 +1,7 @@
+#ifndef ASBESTOS_KERNEL_KEYBOARD_H_
+#define ASBESTOS_KERNEL_KEYBOARD_H_
+
+void keyboard_initialize();
+char keyboard_read();
+
+#endif  // ASBESTOS_KERNEL_KEYBOARD_H_
