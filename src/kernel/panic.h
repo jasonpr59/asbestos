d4aaf7245d447e69b333c5b8127a7b4642142334
Jason P-R <jasonpr59@gmail.com>
2014-12-25 22:39:02 -0500

    Allow panic to be called with just the string and no other args.

--- src/kernel/panic.h
+++ src/kernel/panic.h
@@ -3,7 +3,7 @@
 
 #include <stdarg.h>
 
-#define panic(message, ...) _panic(__FILE__, __LINE__, __func__, message, __VA_ARGS__)
+#define panic(message, ...) _panic(__FILE__, __LINE__, __func__, message, ##__VA_ARGS__)
 
 void _panic(const char *file, int line, const char *function, char *message, ...);
 
