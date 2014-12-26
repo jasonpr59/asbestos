c8a32794d83ba6caa07fe61a7b8cac1ee411f659
Jason P-R <jasonpr59@gmail.com>
2014-12-26 00:47:47 -0500

    Validate the handoff from the multiboot bootloader to the OS.
    This means:
      * Save the handoff-time registers in boot.s.
      * Pass them to kernel_main().
      * Validate that the EAX register contained the multiboot handoff
        magic number.

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,5 +1,8 @@
+#include <x86.h>
 #include "cprintf.h"
 #include "monitor.h"
+#include "multiboot.h"
+#include "panic.h"
 #include "keyboard.h"
 #include "serial.h"
 #include "vga.h"
@@ -10,8 +13,17 @@ void kernel_initialize_terminal() {
   keyboard_initialize();
 }
 
-void kernel_main() {
+void kernel_validate_multiboot_handoff(uint32_t handoff_eax) {
+  if (handoff_eax != MULTIBOOT_HANDOFF_EAX) {
+    panic("[MULTIBOOT] Bad handoff %%eax value: 0x%08x.\n",handoff_eax);
+  } else {
+    cprintf("Multiboot handoff completed successfully.\n");
+  }
+}
+
+void kernel_main(struct PushedRegisters registers) {
   kernel_initialize_terminal();
   cprintf("Starting up Asbestos.\n");
+  kernel_validate_multiboot_handoff(registers.eax);
   run_monitor();
 }
