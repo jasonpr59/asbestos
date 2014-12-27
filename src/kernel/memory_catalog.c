7029d5dff8b3d93698041e1936bbed65d4edd46a
Jason P-R <jasonpr59@gmail.com>
2014-12-27 00:14:43 -0500

    Build up the memory catalog!
    All available memory is added to free_list in memory_catalog.c.

--- src/kernel/memory_catalog.c
+++ src/kernel/memory_catalog.c
@@ -1,27 +1,69 @@
+#include "cprintf.h"
+#include "link_address.h"
 #include "memory_catalog.h"
+#include "panic.h"
+
+// The free list initially includes a block for every usable bit of memory,
+// except for memory that the linker already allocated for the kernel.
+// Then, it is updated as clients allocated and free memory.
+static struct MemoryCatalogBlock *free_list = NULL;
+
+// Only used during initialization.
+static struct MemoryCatalogBlock *next_unused_block =
+    (struct MemoryCatalogBlock *) mem_catalog_start;
+
+void catalog_memory_block(uintptr_t address) {
+  if (address >= (uintptr_t) kernel_start
+      && address < (uintptr_t) kernel_end) {
+    // We're (possibly) already using this page.  Do not add it
+    // allocation system.
+    return;
+  }
+  cprintf(".");
+  struct MemoryCatalogBlock *block = next_unused_block++;
+  block->address = address;
+  // Push it onto the free list.
+  block->next = free_list;
+  free_list = block;
+}
+
+// Rounds an address up to a block boundary.
+// Requires that the block size is a power of two.
+uintptr_t aligned_up(uintptr_t address, size_t block_size) {
+  return (address + block_size - 1) & ~(block_size - 1);
+}
+
+// Adds a range of memory to the free list.
+// Requries that base_address is kMemoryCatalogBlockSize aligned.
+void catalog_memory_range(uintptr_t base_address, size_t length) {
+  cprintf("Adding range [0x%08x, ...) to free list.\n", base_address);
+  for (size_t offset = 0; offset < length; offset += kMemoryCatalogBlockSize) {
+    catalog_memory_block(base_address + offset);
+  }
+}
 
 void memory_catalog_initialize(struct MultibootInfo *multiboot_info) {
-  cprintf("Multiboot reports %dKB of low memory and %dMB of high memory.\n",
-	  multiboot_info->mem_lower, multiboot_info->mem_upper / 1024);
+  int memory_size_kibibytes = (multiboot_info->mem_lower +
+			       multiboot_info->mem_upper);
+  cprintf("Found %dMB of usable RAM.\n", memory_size_kibibytes / 1024);
 
   size_t mmap_length = (size_t) multiboot_info->mmap_length;
   uintptr_t mmap_addr = (uintptr_t) multiboot_info->mmap_addr;
-  cprintf("Looking up physical mmap at 0x%08x, with length 0x%x.\n",
-	  mmap_addr, mmap_length);
 
   size_t offset = 0;
   while (offset < mmap_length) {
     struct MultibootMemoryMapEntry *mmap_entry =
       (struct MultibootMemoryMapEntry *) (mmap_addr + offset);
-    cprintf("Size: %d\n", mmap_entry->size);
-    cprintf("Addr: 0x%08x\n", mmap_entry->base_addr);
-    cprintf("Length: 0x%08x\n", mmap_entry->length);
     if (mmap_entry->type == MULTIBOOT_MEMORY_USABLE) {
-      cprintf("Usable.\n");
-    } else {
-      cprintf("Unusable.\n");
+      uintptr_t base_addr = mmap_entry->base_addr;
+      if (base_addr % kMemoryCatalogBlockSize) {
+	// TODO(jasonpr): Don't panic. Just round up, lessen the
+	// length accordingly, and carry on.
+	panic("Got misaligned memory mapping from bootloader: 0x%0x.\n",
+	      base_addr);
+      }
+      catalog_memory_range(base_addr, mmap_entry->length);
     }
-    cprintf("\n");
     // Skip over the size field, and then the rest of the entry.
     offset += sizeof(mmap_entry->size) + mmap_entry->size; 
   }
--- src/kernel/memory_catalog.h
+++ src/kernel/memory_catalog.h
@@ -1,7 +1,15 @@
 #ifndef ASBESTOS_KERNEL_MEMORY_CATALOG_H_
 #define ASBESTOS_KERNEL_MEMORY_CATALOG_H_
 
+#include <stddef.h>
 #include "multiboot.h"
+
+static const size_t kMemoryCatalogBlockSize = 1 << 12;
+
+struct MemoryCatalogBlock {
+  uintptr_t address;
+  struct MemoryCatalogBlock *next;
+};
 
 void memory_catalog_initialize(struct MultibootInfo *multiboot_info);
 
