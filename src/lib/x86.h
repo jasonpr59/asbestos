414633467104acfb4e0e7939cbc8cb40eba76259
Jason Paller-Rzepka <jasonpr@mit.edu>
2015-01-19 12:25:06 -0500

    Add functions for modifying segment selectors.

--- src/lib/x86.h
+++ src/lib/x86.h
@@ -5,6 +5,18 @@
 
 // This file is heavily influenced by JOS's inc/x86.h.
 
+// TODO(jasonpr): Determine whether this belongs in another file.
+struct SegmentSelector {
+  // What privileges the CPU must require to use this segment.
+  // If the referenced descriptor has a stricter privilege level,
+  // then that constraint will be enforced instead.
+  unsigned int requested_privilege_level : 2;
+  // Which table contains the segment. 0 = GDT, 1 = LDT.
+  unsigned int local : 1;
+  // The index into the descriptor table.
+  unsigned int index : 13;
+} __attribute__((packed));
+
 // Usually, we discourage inlining via command line flags. But, we
 // want our instruction wrapper functions to be inlined.  So, we mark
 // all of them with always_inline.
@@ -12,6 +24,13 @@
 static __inline uintptr_t get_ebp() __attribute__((always_inline));
 static __inline uint8_t inb(uint16_t io_port) __attribute__((always_inline));
 static __inline void outb(uint16_t io_port, uint8_t data) __attribute__((always_inline));
+static __inline void load_cs(struct SegmentSelector selector) __attribute__((always_inline));
+static __inline void load_ds(struct SegmentSelector selector) __attribute__((always_inline));
+static __inline void load_es(struct SegmentSelector selector) __attribute__((always_inline));
+static __inline void load_fs(struct SegmentSelector selector) __attribute__((always_inline));
+static __inline void load_gs(struct SegmentSelector selector) __attribute__((always_inline));
+static __inline void load_ss(struct SegmentSelector selector) __attribute__((always_inline));
+
 
 // Get the current base pointer.
 
@@ -38,6 +57,52 @@ static void outb(uint16_t io_port, uint8_t data) {
   __asm __volatile("outb %0, %1" :
 		   /* No output. */ : 
 		   "a" (data), "d" (io_port));
+}
+
+static void load_cs(struct SegmentSelector selector) {
+  // Change the CS with a long return.
+  // Thanks to Owen at http://f.osdev.org/viewtopic.php?f=1&t=22641
+
+  // Pad the 16-bit selector to 32 bits so it can be pushed onto the
+  // stack.
+  uint32_t padded_selector = *((uint16_t *) &selector);
+
+  __asm __volatile("push %0\n\t"
+		   "push $flush_cs\n\t"
+		   "retf\n\t"
+		   "flush_cs:" :
+		   /* No output. */ :
+		   "r" (padded_selector));
+}
+
+static void load_ds(struct SegmentSelector selector) {
+  __asm __volatile("mov %0, %%ds" :
+		   /* No output. */ :
+		   "r" (selector));
+}
+
+static void load_es(struct SegmentSelector selector) {
+  __asm __volatile("mov %0, %%es" :
+		   /* No output. */ :
+		   "r" (selector));
+}
+
+static void load_fs(struct SegmentSelector selector) {
+  __asm __volatile("mov %0, %%fs" :
+		   /* No output. */ :
+		   "r" (selector));
+}
+
+static void load_gs(struct SegmentSelector selector) {
+  __asm __volatile("mov %0, %%gs" :
+		   /* No output. */ :
+		   "r" (selector));
+}
+
+static void load_ss(struct SegmentSelector selector) {
+  __asm __volatile("mov %0, %%ss" :
+		   /* No output. */ :
+		   "r" (selector));
 }
 
 struct PushedRegisters {
