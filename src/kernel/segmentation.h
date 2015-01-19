dca7610510e2903dfc439eb081f4fe46f57f8c92
Jason Paller-Rzepka <jasonpr@mit.edu>
2015-01-19 11:06:43 -0500

    Add SegmentSelector struct.

--- src/kernel/segmentation.h
+++ src/kernel/segmentation.h
@@ -39,4 +39,15 @@ struct SegmentDescriptor {
   uint8_t base_31_24;
 } __attribute__((packed));
 
+struct SegmentSelector {
+  // What privileges the CPU must require to use this segment.
+  // If the referenced descriptor has a stricter privilege level,
+  // then that constraint will be enforced instead.
+  unsigned int requested_privilege_level : 2;
+  // Which table contains the segment. 0 = GDT, 1 = LDT.
+  unsigned int local : 1;
+  // The index into the descriptor table.
+  unsigned int index : 13;
+} __attribute__((packed));
+
 #endif  // ASBESTOS_KERNEL_SEGMENT_H_
