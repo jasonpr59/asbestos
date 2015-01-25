1227de710b672a96b3db5fe8337f0d1e55f1059e
Jason Paller-Rzepka <jasonpr@mit.edu>
2015-01-25 02:15:13 -0500

    Print out the name of the interrupt.
    This is the beginnings of the interrupt dispatch mechanism.

--- src/kernel/interrupts.c
+++ src/kernel/interrupts.c
@@ -6,7 +6,6 @@
 #include "segmentation.h"
 
 static struct InterruptDescriptor idt[IDT_SIZE];
-
 static struct PseudoDescriptor idt_descriptor = {
   .limit = sizeof(struct InterruptDescriptor) * IDT_SIZE - 1,
   .base = (uintptr_t) &idt
@@ -24,8 +23,62 @@ struct InterruptDescriptor idt_entry(uintptr_t handler_pointer) {
 }
 
 void handle_interrupt(int interrupt) {
-  disable_interrupts();
-  panic("Got interrupt %d.\n", interrupt);
+  switch (interrupt) {
+  case INTERRUPT_DIVIDE_ERROR:
+    panic("Divide error.\n");
+    break;
+  case INTERRUPT_DEBUG_EXCEPTION:
+    panic("Debug error.\n");
+    break;
+  case INTERRUPT_NMI:
+    panic("Non-maskable interrupt.\n");
+    break;
+  case INTERRUPT_BREAKPOINT:
+    panic("Breakpoint.\n");
+    break;
+  case INTERRUPT_OVERFLOW:
+    panic("Overflow.\n");
+    break;
+  case INTERRUPT_BOUNDS_CHECK:
+    panic("Failed bounds check.\n");
+    break;
+  case INTERRUPT_INVALID_OPCODE:
+    panic("Invalid opcode.\n");
+    break;
+  case INTERRUPT_COPROCESSOR_NOT_AVAILABLE:
+    panic("Coprocessor not available.\n");
+    break;
+  case INTERRUPT_DOUBLE_FAULT:
+    panic("Double fault.\n");
+    break;
+  case INTERRUPT_RESERVED_9:
+    panic("Reserved interrupt (9).\n");
+    break;
+  case INTERRUPT_INVALID_TSS:
+    panic("Invalid TSS.\n");
+    break;
+  case INTERRUPT_SEGMENT_NOT_PRESENT:
+    panic("Segment not present.\n");
+    break;
+  case INTERRUPT_STACK_EXCEPTION:
+    panic("Stack exception.\n");
+    break;
+  case INTERRUPT_GENERAL_PROTECTION_FAULT:
+    panic("General protection fault.\n");
+    break;
+  case INTERRUPT_PAGE_FAULT:
+    panic("Page fault.\n");
+    break;
+  case INTERRUPT_RESERVED_15:
+    panic("Reserved interrupt (15).\n");
+    break;
+  case INTERRUPT_COPROCESSOR_ERROR:
+    panic("Coprocessor error.\n");
+    break;
+  default:
+    panic("Unidentified interrupt: %d.\n", interrupt);
+    break;
+  }
 }
 
 void setup_idt() {
