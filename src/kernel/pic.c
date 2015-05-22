12eb3fc6b91d4b1f57b5235c340d020df62ffe60
Jason Paller-Rzepka <jasonpr@mit.edu>
2015-05-22 00:44:02 -0400

    Add pic_send_eoi().
    When we write handlers for the hardware interrupts, we will need to
    send EOIs to the PIC(s), so they can continue fielding interrupts.

--- src/kernel/pic.c
+++ src/kernel/pic.c
@@ -1,4 +1,6 @@
+#include <stdint.h>
 #include <x86.h>
+#include "interrupts.h"
 #include "pic.h"
 
 #define PIC_MASTER 0x20
@@ -9,10 +11,19 @@
 #define PIC_SLAVE_CMD PIC_SLAVE
 #define PIC_SLAVE_DATA (PIC_SLAVE + 1)
 
+// "Operation Command Words."
+#define PIC_OCW2_EOI 0x20
+
 void disable_pics() {
   // Write a fully set bitmask to both PICs, so no hardware interrupts
   // come through.
   // TODO(jasonpr): Move PIC-related code elsewhere.
   outb(PIC_MASTER_DATA, 0xFF);
   outb(PIC_SLAVE_DATA, 0xFF);
+}
+void pic_send_eoi() {
+  // For now, assume the the interrupt came from the master, and
+  // only send the eoi to the master.
+  // TODO(jasonpr): Handle slave-initiated interrupts.
+  outb(PIC_MASTER_CMD, PIC_OCW2_EOI);
 }
--- src/kernel/pic.h
+++ src/kernel/pic.h
@@ -1,4 +1,5 @@
 #ifndef ASBESTOS_KERNEL_PIC_H_
 #define ASBESTOS_KERNEL_PIC_H_
 void disable_pics();
+void pic_send_eoi();
 #endif  // ASBESTOS_KERNEL_PIC_H_
