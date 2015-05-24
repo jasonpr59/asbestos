92a46fa72cb84c981ece7f38ee35e1b67f2929fa
Jason Paller-Rzepka <jasonpr@mit.edu>
2015-05-24 15:22:05 -0400

    Prepare more general `$INPUT_read()` interface.

--- src/kernel/input.h
+++ src/kernel/input.h
@@ -3,6 +3,19 @@
 
 #include <stddef.h>
 
+#define ERR_INPUT_BUFFER_FULL -1
+
+// Return values for `$INPUT_read()` functions:
+
+// The call to a input read function failed because there is no more
+// input to consume at the moment.
+#define INPUT_EXHAUSTED -1
+// The call to an input read function did not produce any data.  There
+// still may be more input to consume.  (For example, the function
+// might have read a key-up event, which produces no character data.)
+#define INPUT_RETRY -2
+
+
 int get_keyboard_input();
 int get_serial_input();
 char input_character();
