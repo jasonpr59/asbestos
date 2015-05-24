8167ebf6efcf98d3a9c4d869b960a20421104f9b
Jason Paller-Rzepka <jasonpr@mit.edu>
2015-05-24 14:53:12 -0400

    Expose serial_can_read().
    The input library will use this function so that it can consume all
    the available input, but never hang.

--- src/kernel/serial.h
+++ src/kernel/serial.h
@@ -1,8 +1,11 @@
 #ifndef SERIAL_H_
 #define SERIAL_H_
 
+#include <stdbool.h>
+
 void serial_initialize();
 void serial_write(char character);
 char serial_read();
+bool serial_can_read();
 
 #endif  // SERIAL_H_
