63fdc47e3c9e10729cdf1527748999f57746c737
Jason Paller-Rzepka <jasonpr@google.com>
2017-06-03 15:31:07 -0400

    Add commands to kernel monitor.
    Now you can ask for "version" or "backtrace".
    
    Neither of these is particularly interesting (Asbestos has no versions
    yet, and the backtrace is both poorly formatted and unchanging).
    Still, it's progress.  Some day we'll add some useful commands.

--- src/kernel/monitor.c
+++ src/kernel/monitor.c
@@ -1,18 +1,27 @@
 #include "monitor.h"
+#include "backtrace.h"
 #include "cprintf.h"
 #include "input.h"
+#include "string.h"
 
 #define PROMPT_SIZE 100
 
 void run_monitor() {
   cprintf("Started the Asbestos Kernel Monitor.\n");
-  cprintf("Currently, this is just an echo prompt.\n");
+  cprintf("Commands: backtrace, version.\n");
 
   char prompt[PROMPT_SIZE];
   while (1) {
     cprintf("$ ");
     input_string(prompt, PROMPT_SIZE);
-    cprintf(prompt);
+    if (!strcmp("version", prompt)) {
+      cprintf("Asbestos v0.0 (no version numbers yet!)\n");
+    } else if (!strcmp("backtrace", prompt)) {
+      cprintf("Backtrace requested.\n");
+      print_backtrace();
+    } else {
+      cprintf("Unknown command: %s", prompt);
+    }
     cprintf("\n");
   }
 }
